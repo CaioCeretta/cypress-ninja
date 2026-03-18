20e31c65d978f08c0fa83cf45008a29812e56f5f
CaioCeretta <caiceretta@gmail.com>
2026-03-17 23:24:18 -0300

    chore: finish cypress introduction

--- webdojo/cypress/e2e/login.cy.js
+++ webdojo/cypress/e2e/login.cy.js
@@ -5,5 +5,42 @@ describe("login", () => {
 
     cy.get("#email").type("papito@webdojo.com");
     cy.get("#password").type("katana123");
+
+    cy.contains("button", "Entrar").click();
+
+    cy.get('[data-cy="user-name"]')
+      .should("be.visible")
+      .and("have.text", "Fernando Papito");
+
+    cy.get('[data-cy="welcome-message"]')
+      .should("be.visible")
+      .and(
+        "have.text",
+        "Olá QA, esse é o seu Dojo para aprender Automação de Testes.",
+      );
+  });
+
+  it("should not login with invalid password", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("localhost:3000");
+
+    cy.get("#email").type("papito@webdojo.com");
+    cy.get("#password").type("katana321");
+
+    cy.contains("button", "Entrar").click();
+
+    cy.contains("Acesso negado! Tente novamente.").should("be.visible");
+  });
+
+  it("should not login with non-registered email", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("localhost:3000");
+
+    cy.get("#email").type("papitoo@webdojo.com");
+    cy.get("#password").type("katana123");
+
+    cy.contains("button", "Entrar").click();
+
+    cy.contains("Acesso negado! Tente novamente.").should("be.visible");
   });
 });
